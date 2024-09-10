e6f9cc0801f1f0af161ebf7e4318bc14c1f011aa
Gabriel Enríquez <gabrielenlo9@gmail.com>
2024-09-10 10:47:09 +0200

    HTML , CSS and Js of my memory game.

---
+++ js/memoria.js
@@ -0,0 +1,101 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const matches = document.getElementById("matches");
+  const mistakes = document.getElementById("mistakes");
+  const totalClics = document.getElementById("totalClics");
+  const timer = document.getElementById("timer");
+  const startGameButton = document.getElementById("start-game");
+  const gameBoard = document.getElementById("game-board");
+
+  let firstCard = null;
+  let secondCard = null;
+  let lock = false;
+
+  startGameButton.addEventListener("click", startGame);
+
+  function startGame() {
+    matches.textContent = "Aciertos: 0";
+    mistakes.textContent = "Errores: 0";
+    totalClics.textContent = "Total de Clics: 0";
+    timer.textContent = "Tiempo 00:00";
+    gameBoard.innerHTML = "";
+
+    const images = [
+      "fisio.jpg",
+      "fisio.jpg",
+      "sem.jpg",
+      "sem.jpg",
+      "seo.jpg",
+      "seo.jpg",
+      "sushi.jpg",
+      "sushi.jpg",
+      "tacos.jpg",
+      "tacos.jpg",
+      "tarta.jpg",
+      "tarta.jpg",
+      "taza-cafe.jpg",
+      "taza-cafe.jpg",
+      "wordpress.jpg",
+      "wordpress.jpg",
+    ];
+
+    shuffle(images);
+
+    for (let i = 0; i < images.length; i++) {
+      let card = document.createElement("div");
+      card.classList.add("card");
+      card.dataset.image = images[i];
+
+      let img = document.createElement("img");
+      img.src = "img/carta.jpg";
+      card.appendChild(img);
+
+      card.addEventListener("click", flipCard);
+
+      gameBoard.appendChild(card);
+    }
+  }
+
+  function flipCard() {
+    if (lock) return;
+
+    const card = this;
+    if (card.dataset.flipped === "true") return;
+
+    let img = card.querySelector("img");
+    img.src = `img/${card.dataset.image}`;
+    card.dataset.flipped = "true";
+
+    if(!firstCard){
+        firstCard = card;
+    }else{
+        secondCard = card;
+        lock = true;
+
+        setTimeout(checkForMath, 1000);
+    }
+
+  }
+
+  function checkForMath(){
+    if(firstCard.dataset.image === secondCard.dataset.image){
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    }else{
+        firstCard.querySelector('img').src = 'img/carta.jpg';
+        secondCard.querySelector('img').src= 'img/carta.jpg';
+        firstCard.dataset.flipped = 'false';
+        secondCard.dataset.flipped = 'false';
+    }
+
+    firstCard = null;
+    secondCard = null;
+    lock= false;
+  }
+
+  function shuffle(array) {
+    for (i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+});
