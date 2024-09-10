17e12bb1e95dbe320eccec5cd4cd29682ca11a69
Gabriel Enríquez <gabrielenlo9@gmail.com>
2024-09-10 18:53:27 +0200

    comments added

--- js/memoria.js
+++ js/memoria.js
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", () => {
   const startGameButton = document.getElementById("start-game");
   const gameBoard = document.getElementById("game-board");
 
-  let firstCard = null;
-  let secondCard = null;
-  let lock = false;
+  let firstCard = null; //Almacena la primera carta seleccionada en cada turno
+  let secondCard = null; //Almacena la segunda carta seleccioanda en cada turno
+  let lock = false; // Evita que se volteen más de dos cartas al mismo tiempo
   let matchCount = 0;
   let mistakesCount = 0;
   let clicCount = 0;
@@ -16,15 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
   let interval;
   let totalPairs = 8;
 
+
+//Evento de inicio de juego añadido a botón
   startGameButton.addEventListener("click", startGame);
 
+//Método para iniciar el juego 
   function startGame() {
     matches.textContent = "Aciertos: 0";
     mistakes.textContent = "Errores: 0";
     totalClics.textContent = "Total de Clics: 0";
     timer.textContent = "Tiempo 00:00";
     gameBoard.innerHTML = "";
-
+//Reseteo de contadores
     clicCount= 0;
     matchCount = 0;
     mistakesCount= 0;
@@ -32,8 +35,8 @@ document.addEventListener("DOMContentLoaded", () => {
     startTime = Date.now();
 
     const images = [
-      "fisio.jpg",
-      "fisio.jpg",
+      "persona.jpg",
+      "persona.jpg",
       "sem.jpg",
       "sem.jpg",
       "seo.jpg",
@@ -49,9 +52,14 @@ document.addEventListener("DOMContentLoaded", () => {
       "wordpress.jpg",
       "wordpress.jpg",
     ];
-
+    //Se baraja el array de imágenes 
     shuffle(images);
 
+    /* Para cada imagen, se crea un div con la clase 'card' y se asocia la imagen correspondiente.
+    *  Dentro de cada div se inserta una imagen con 'carta.jpg' (cara oculta inicial).
+    *  Luego, se añade un evento 'click' que llamará a la función 'flipCard' al hacer clic.
+    */
+  
     for (let i = 0; i < images.length; i++) {
       let card = document.createElement("div");
       card.classList.add("card");
@@ -66,8 +74,18 @@ document.addEventListener("DOMContentLoaded", () => {
       gameBoard.appendChild(card);
     }
 
+    //El metodo updateTimer se actualiza cada segundo para obtener el tiempo que ha transcurrido desde el inicio de la partida.
     interval = setInterval(updateTimer, 1000);
   }
+
+
+   /* Función que voltea la carta.
+  *  Si 'lock' es verdadero, impide que se volteen más cartas.
+  *  Si la carta ya fue volteada (propiedad 'flipped'), también se sale.
+  *  Aumenta el número de clics, muestra la imagen de la carta y la marca como volteada.
+  *  Si no hay una primera carta seleccionada, se asigna a 'firstCard'.
+  *  Si ya hay una, se asigna a 'secondCard' y se bloquean más clics con 'lock' hasta que se chequeen.
+  */
 
   function flipCard() {
     if (lock) return;
@@ -87,10 +105,15 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       secondCard = card;
       lock = true;
-
+     // Llama a 'checkForMatch' después de 1 segundo para verificar si las dos cartas coinciden
       setTimeout(checkForMatch, 1000);
     }
   }
+
+/* Función que verifica si las dos cartas seleccionadas coinciden.
+  *  Si coinciden, incrementa el contador de aciertos, elimina los eventos de clic y verifica si se han encontrado todas las parejas.
+  *  Si no coinciden, incrementa el contador de errores y las voltea de nuevo.
+  */
 
   function checkForMatch() {
     if (firstCard.dataset.image === secondCard.dataset.image) {
@@ -106,7 +129,7 @@ document.addEventListener("DOMContentLoaded", () => {
             "Has conseguido encontrar todas las parejas. ¿Quieres echar otra partida?"
           );
           if (playAgain) {
-            startGame();
+            startGame(); // Reinicia el juego si el jugador acepta
           }
         }, 100);
       }
@@ -119,17 +142,20 @@ document.addEventListener("DOMContentLoaded", () => {
       secondCard.dataset.flipped = "false";
     }
 
-    firstCard = null;
+    firstCard = null; // Resetea las cartas seleccionadas
     secondCard = null;
-    lock = false;
+    lock = false; // Desbloquea el volteo de cartas
   }
 
+   // Algoritmo de Fisher-Yates para barajar el array de imágenes
   function shuffle(array) {
     for (i = array.length - 1; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
   }
+
+  //Función para actualizar el tiempo en minutos y segundos
 
   function updateTimer() {
     let gameTime = Math.floor((Date.now() - startTime) / 1000);
